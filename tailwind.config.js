cc4648e674bfc2d991da501c900a001fc7df64da
milad ramavandi <miladramavandi@milads-MacBook-Air.local>
2024-12-10 11:02:13 +0330

    tailwind.config.js updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 import { nextui } from "@nextui-org/react";
 
-module.exports = {
+const config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,3 +11,5 @@ module.exports = {
   theme: {},
   plugins: [nextui()],
 };
+
+export default config
